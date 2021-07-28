599b29782860bb5652e6316ba3f705f2d4b1f383
s-manguy <79250100+s-manguy@users.noreply.github.com>
2021-07-28 10:27:08 +0200

    Add files via upload

---
+++ APIs-microservices/fcc-02-requestheaderparser/server.js
@@ -0,0 +1,49 @@
+// server.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// to get the full ipaddress
+app.enable('trust proxy');
+
+app.get("/api/whoami", (req, res) => {
+  /* one solution */
+ // const ipaddress = req.ip;
+  //const language = req.acceptsLanguages();
+  //const software = req.get("user-agent");
+  //res.json({ipaddress, language, software});
+
+  /* another solution */
+  res.json({ 
+    "ipaddress": req.ip, 
+    "language": req.acceptsLanguages(), 
+    "software": req.get("user-agent")
+  });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
